d9eb2a2d47dbc69c87bb8edd2a1c037a8a48afa9
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2018-01-06 11:06:01 +0100

    setup of the db

--- HaaS.py
+++ HaaS.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sealdarethebest'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///config/haas.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    admin = db.Column(db.Boolean)
+    hashes = db.relationship('Hash', backref='user', lazy=True)
+
+class Hash(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hash = db.Column(db.String(255), unique=True)
+    created_at = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 @app.route('/')
