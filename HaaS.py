5672c098fb2a881be3f76acd792f9d511ad27196
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2018-01-06 11:50:30 +0100

    login route done

--- HaaS.py
+++ HaaS.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
+import jwt
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sealdarethebest'
@@ -43,6 +45,25 @@ def create_user():
     db.session.commit()
 
     return jsonify({'message': 'New user created'})
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(email=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+
+        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
 
 if __name__ == '__main__':
